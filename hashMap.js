d53db14071c68e3d9e240387e8af3d01dea42546
dejanpls <68787353+dejanpls@users.noreply.github.com>
2025-05-25 11:58:07 +0200

    feat: implement set method which takes in key, value pair and saves it in the hash-based index in the backet.

--- hashMap.js
+++ hashMap.js
@@ -1,10 +1,12 @@
 export default class hashMap {
   #load_factor;
   #capacity;
+  #buckets;
 
   constructor(load_factor, capacity) {
     this.#load_factor = load_factor;
     this.#capacity = capacity;
+    this.#buckets = Array.from({ length: this.#capacity }, () => []);
   }
 
   hash(key) {
@@ -16,5 +18,21 @@ export default class hashMap {
     }
 
     return hashCode;
+  }
+
+  set(key, value) {
+    const index = this.hash(key);
+    const bucket = this.#buckets[index];
+
+    for (let i = 0; i < bucket.length; i++) {
+      const [storedKey, storedValue] = bucket[i];
+
+      if (storedKey === key) {
+        bucket[i][1] = value;
+        return;
+      }
+    }
+
+    bucket.push([key, value]);
   }
 }
